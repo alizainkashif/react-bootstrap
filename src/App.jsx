8210a5a49016dc3a944a58c73d0c4bdc8b7fed9d
alizainkashif773@gmail.com <alizainkashif773@gmail.com>
2024-06-09 09:02:45 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <>
+    <h1 className='text-center p-8 text-bg-dark' > Ali Zain Kashif</h1>
+    </>
+  )
+}
+
+export default App
